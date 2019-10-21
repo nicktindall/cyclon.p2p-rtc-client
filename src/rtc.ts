b4c8ed2846595a8f00bd5c984e3af6c05999d843
Nick Tindall <nick@tindall.id.au>
2019-10-21 21:54:01 +1100

    Port RTC over to async/await, remove bluebird dependency

--- src/rtc.ts
+++ src/rtc.ts
@@ -1,10 +1,10 @@
-import {Promise} from 'bluebird';
 import {EventEmitter} from 'events';
-import {OfferMessage, SignallingService} from "./SignallingService";
-import {ChannelFactory} from "./ChannelFactory";
-import {MetadataProvider} from "cyclon.p2p";
-import {WebRTCCyclonNodePointer} from "./WebRTCCyclonNodePointer";
-import {Channel} from "./Channel";
+import {OfferMessage, SignallingService} from './SignallingService';
+import {ChannelFactory} from './ChannelFactory';
+import {MetadataProvider} from 'cyclon.p2p';
+import {WebRTCCyclonNodePointer} from './WebRTCCyclonNodePointer';
+import {Channel} from './Channel';
+import {TimeoutError} from 'cyclon.p2p-common';
 
 export class RTC extends EventEmitter {
 
@@ -21,7 +21,7 @@ export class RTC extends EventEmitter {
     connect(metadataProviders: { [key: string]: MetadataProvider }, rooms: string[]) {
         if (!this.connected) {
             this.signallingService.connect(metadataProviders, rooms);
-            this.signallingService.on("offer", (message: OfferMessage) => {
+            this.signallingService.on('offer', (message: OfferMessage) => {
                 this.handleOffer(message);
             });
             this.connected = true;
@@ -40,54 +40,58 @@ export class RTC extends EventEmitter {
         this.channelListeners[type] = callback;
     }
 
-    openChannel(type: string, remotePointer: WebRTCCyclonNodePointer): Promise<Channel> {
+    async openChannel(type: string, remotePointer: WebRTCCyclonNodePointer): Promise<Channel> {
         const channel = this.channelFactory.createChannel(remotePointer);
 
-        return channel.createOffer(type)
-            .then(() => channel.sendOffer())
-            .then(() => channel.startListeningForRemoteIceCandidates())
-            .then(() => channel.waitForAnswer())
-            .then((answerMessage) => channel.handleAnswer(answerMessage))
-            .then(() => channel.startSendingIceCandidates())
-            .then(() => channel.waitForChannelToOpen())
-            .then(() => channel.stopSendingIceCandidates())
-            .catch(function (error) {
-                // If an error occurs here, cleanup our attempted channel
-                // establishment resources before continuing
-                channel.close();
-                throw error;
-            });
+        try {
+            await channel.createOffer(type);
+            await channel.sendOffer();
+            await channel.startListeningForRemoteIceCandidates();
+            const answerMessage = await channel.waitForAnswer();
+            await channel.handleAnswer(answerMessage);
+            await channel.startSendingIceCandidates();
+            await channel.waitForChannelToOpen();
+            await channel.stopSendingIceCandidates();
+            return channel;
+        } catch (error) {
+            // If an error occurs here, cleanup our attempted channel
+            // establishment resources before continuing
+            channel.close();
+            throw error;
+        }
     }
 
-    private handleOffer(offerMessage: OfferMessage) {
+    private async handleOffer(offerMessage: OfferMessage): Promise<void> {
         const channelType = offerMessage.channelType;
         const listener: (channel: Channel) => unknown = this.channelListeners[channelType];
         const remotePointer = offerMessage.sourcePointer;
         const correlationId = offerMessage.correlationId;
 
-        this.emit("offerReceived", channelType, offerMessage.sourcePointer);
+        this.emit('offerReceived', channelType, offerMessage.sourcePointer);
 
         if (listener) {
             const channel = this.channelFactory.createChannel(remotePointer, correlationId);
-
-            channel.createAnswer(offerMessage.sessionDescription)
-                .then(() => channel.startListeningForRemoteIceCandidates())
-                .then(() => channel.sendAnswer())
-                .then(() => channel.startSendingIceCandidates())
-                .then(() => channel.waitForChannelEstablishment())
-                .then(() => channel.waitForChannelToOpen())
-                .then(() => channel.stopSendingIceCandidates())
-                .then(listener)
-                .catch(Promise.TimeoutError, () => {
-                    this.emit("incomingTimeout", channelType, offerMessage.sourcePointer);
+            try {
+                await channel.createAnswer(offerMessage.sessionDescription);
+                await channel.startListeningForRemoteIceCandidates();
+                await channel.sendAnswer();
+                await channel.startSendingIceCandidates();
+                await channel.waitForChannelEstablishment();
+                await channel.waitForChannelToOpen();
+                await channel.stopSendingIceCandidates();
+                await listener(channel);
+            } catch (error) {
+                if (error instanceof TimeoutError) {
+                    this.emit('incomingTimeout', channelType, offerMessage.sourcePointer);
                     channel.close();
-                })
-                .catch((e: Error) => {
-                    this.emit("incomingError", channelType, offerMessage.sourcePointer, e);
+                } else {
+                    this.emit('incomingError', channelType, offerMessage.sourcePointer, error);
                     channel.close();
-                });
+                }
+                throw error;
+            }
         } else {
-            console.warn("No listener for channel type " + channelType + ", ignoring offer!");
+            console.warn('No listener for channel type ' + channelType + ', ignoring offer!');
         }
     }
 }
